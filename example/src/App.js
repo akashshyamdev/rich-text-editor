f5a6d4d8137843e7f8005af16c2d5a6cc93501af
Akash Shyam <shyamiy@gmail.com>
2021-05-29 18:17:18 +0530

    🎉  Intial Setup

---
+++ example/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Test from 'rich-text-editor';
+
+function App() {
+	return (
+		<div className='App'>
+			<h1>Test</h1>
+
+			<Test />
+		</div>
+	);
+}
+
+export default App;
